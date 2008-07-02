f5673d3ac816ac5976d5d7aa6b105b0d6743b1c8
jamie <jamie@b0f43cc7-0151-0410-a95c-c02dbe2a96cd>
2008-07-02 14:38:47 +0000

    git-svn-id: http://www.angelforge.org/subversion/citethis@9 b0f43cc7-0151-0410-a95c-c02dbe2a96cd

--- defaults/preferences/citethis.js
+++ defaults/preferences/citethis.js
@@ -2,4 +2,4 @@ pref("extensions.citethis.boolpref", false);
 pref("extensions.citethis.intpref", 0);
 pref("extensions.citethis.stringpref", "A string");
 // See http://kb.mozillazine.org/Localize_extension_descriptions
-pref("extensions.jamie.ly@gmail.com.description", "chrome://citethis/locale/citethis.properties");
+pref("extensions.jamie.ly@gmail.com.description", "chrome://citethis/chrome/locale/citethis.properties");
